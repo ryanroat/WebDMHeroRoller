bd66e2c2e23f37ef74038a1aac2899e605ec7342
Ryan Roat <ryanroat@gmail.com>
2018-01-27 17:09:27 -0500

    JS to drop lowest die

--- wdmhr.js
+++ wdmhr.js
@@ -4,9 +4,21 @@ function rando(max) {
 
 function sortRolls(dice) {
   let n = dice.length;
-  let c = 0;
+  let ae = n - 1; //array end (length - 1)
+  let swap,die = 0;
   
+  for (let i=0;i<n;i++) {
+    die = dice[i];
+    for (let j = i + 1; j < n; j++) {
+      if (die < dice[j]) {
+        swap = die;
+        die = dice[j];
+        dice[j] = swap;
+      }
+    }
+  }
 
+  return dice[0]+dice[1]+dice[2];
 }
 
 function rollAHero() {
@@ -16,17 +28,11 @@ function rollAHero() {
 
   let report = document.getElementById("clickOutput"); 
   report.innerHTML = "Here are your stats in no particular order;<br>click above for another set.";
-  //let rolls = document.getElementsByClassName("rolls");
-
-  for (i=0; i<7; i++){
-    for (j=0; j<4; j++) {
-      d6s [j] = rando(6);
-    }
-
+    for (i=0; i<7; i++){
+      for (j=0; j<4; j++) {
+        d6s [j] = rando(6);
+      } 
     stat = sortRolls(d6s);
-        
-    document.write(d6s,stat);
-
-
+    document.write(d6s," ",stat," ");
   }
 }
